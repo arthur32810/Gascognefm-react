49b3d2fb7cc0ae016e21407ca70382f8fc58c1c6
arthur <arthur32810@gmail.com>
2024-04-17 19:39:25 +0200

    travail sur le footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+
+    return (
+        <footer>
+            <h2>
+                Nos partenaires :
+            </h2>
+
+        </footer>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Home from './pages/Home/Home.jsx'
 import Header from './components/Header/Header.jsx';
+import Footer from './components/Footer/Footer.jsx';
 
 const router = createBrowserRouter([
   {
@@ -13,8 +14,12 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+
     <Header />
+
     <RouterProvider router={router} />
+
+    <Footer />
 
   </React.StrictMode>,
 )
